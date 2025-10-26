5dbbaf779bef68c4026f220833fee7703b3ff97b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-27 03:45:28 +0500

    starting website page

--- components/Effective.tsx
+++ components/Effective.tsx
@@ -6,7 +6,7 @@ import AutomationCard from './AutomationCard'
 const Effective = () => {
   return (
     <section id='automations' className='py-10 max-w-[1320px] mx-auto text-white'>
-        <Title buttonHeading='Automations' firstLine='Most Effective and cost,' secondLine="Time Reduction Automations." para='Some of the most effective and cost time reduction automations that are offered by our consultancy.' />
+        <Title buttonHeading='Automations' firstLine='Most Effective, Cost' secondLine="And Time Reduction Automations." para='Some of the most effective and cost time reduction automations that are offered by our consultancy.' />
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-8 md:px-20 lg:px-28'>
             {automationTools.map((item, i) => (
                 <AutomationCard key={i} {...item} />
